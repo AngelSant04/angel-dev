cc968bb614dc70906499a6bd002905a942b18f8c
AngelDev <angelsantamaria0412@gmail.com>
2024-06-19 02:05:25 -0500

    gg

--- js/download.js
+++ js/download.js
@@ -1,7 +1,7 @@
 document.getElementById("descargarCV").addEventListener("click", function() {
-    var pdfPath = "documentos/CV_ANGEL.pdf";
+    var pdfPath = "documentos/CURRICULUM-2024.pdf";
     var link = document.createElement('a');
     link.href = pdfPath;
-    link.download = "CV_ANGEL.pdf";
+    link.download = "CURRICULUM-2024.pdf";
     link.click();
 });
